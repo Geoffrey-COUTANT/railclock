c1d0daa9f503ae20581939d9fce43df8e5bf9ae5
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2025-02-18 11:32:43 +0100

    added back button

--- app/(details)/index.tsx
+++ app/(details)/index.tsx
@@ -4,27 +4,32 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import {Text, View} from '@/components/Themed';
 import {CornerDownRight} from "lucide-react-native";
 import {red} from "react-native-reanimated/lib/typescript/Colors";
+import {globalStyles} from "@/app/styles"
+import {BackButton} from "@/components/BackButton";
 
 export default function DetailsScreen() {
     return (
-        <View style={styles.container}>
-            <View style={styles.list}>
-                <Image source={require('./images/map.png')} style={styles.headerImage}/>
-                <View style={styles.paddedBox}>
-                    <View style={styles.horizontalList}>
-                        <View style={styles.list}>
-                            <Text style={styles.title}>Ychoux</Text>
-                            <View style={styles.horizontalList}>
-                                <CornerDownRight style={styles.icon} />
-                                <Text style={styles.title}>Bordeaux St Jean</Text>
+        <View style={globalStyles.container}>
+            <View style={globalStyles.list}>
+                <View>
+                    <Image source={require('./images/map.png')} style={globalStyles.headerImage}/>
+                    <BackButton/>
+                </View>
+                <View style={globalStyles.paddedBox}>
+                    <View style={globalStyles.horizontalList}>
+                        <View style={globalStyles.list}>
+                            <Text style={globalStyles.title}>Ychoux</Text>
+                            <View style={globalStyles.horizontalList}>
+                                <CornerDownRight style={globalStyles.iconDim} />
+                                <Text style={globalStyles.title}>Bordeaux St Jean</Text>
                             </View>
                         </View>
-                        <View style={styles.full}>
+                        <View style={globalStyles.full}>
 
                         </View>
-                        <View style={styles.listRight}>
-                            <Text style={styles.hourText}>08:00</Text>
-                            <Text style={styles.textBody}>à l'heure</Text>
+                        <View style={globalStyles.listRight}>
+                            <Text style={globalStyles.hourText}>08:00</Text>
+                            <Text style={globalStyles.textBody}>à l'heure</Text>
                         </View>
                     </View>
                 </View>
@@ -32,55 +37,3 @@ export default function DetailsScreen() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'stretch',
-        flex: 1
-    },
-    full: {
-        flex: 1,
-    },
-    headerImage: {
-        height: 250,
-    },
-    list: {
-        display: "flex",
-    },
-    listRight: {
-        display: "flex",
-        alignItems: "flex-end",
-    },
-    horizontalList: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 5
-    },
-    icon: {
-        color: "black",
-        opacity: 0.5
-    },
-    paddedBox: {
-        padding: 20,
-    },
-    title: {
-        fontSize: 25,
-        fontFamily: "BricolageGrotesqueSemiBold",
-        lineHeight: 30
-    },
-    textBody: {
-        fontSize: 18,
-        fontFamily: "BricolageGrotesqueRegular",
-    },
-    hourText: {
-        fontSize: 30,
-        fontFamily: "BricolageGrotesqueBold",
-        lineHeight: 30
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    }
-});
---
+++ app/styles.tsx
@@ -0,0 +1,56 @@
+﻿import {StyleSheet} from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        alignItems: 'stretch',
+        flex: 1
+    },
+    full: {
+        flex: 1,
+    },
+    headerImage: {
+        height: 250,
+    },
+    list: {
+        display: "flex",
+    },
+    listRight: {
+        display: "flex",
+        alignItems: "flex-end",
+    },
+    horizontalList: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5
+    },
+    iconDim: {
+        color: "black",
+        opacity: 0.5
+    },
+    icon: {
+        color: "black"
+    },
+    paddedBox: {
+        padding: 20,
+    },
+    title: {
+        fontSize: 25,
+        fontFamily: "BricolageGrotesqueSemiBold",
+        lineHeight: 30
+    },
+    textBody: {
+        fontSize: 18,
+        fontFamily: "BricolageGrotesqueRegular",
+    },
+    hourText: {
+        fontSize: 30,
+        fontFamily: "BricolageGrotesqueBold",
+        lineHeight: 30
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    }
+});
