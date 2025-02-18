001428e52df380a67ca7f1036e06bbcec6b4d8ea
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2025-02-18 14:23:05 +0100

    removed dark theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,17 +3,17 @@ const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
+    text: '#210010',
     background: '#fff',
-    tint: tintColorLight,
+    tint: "#AB1E62",
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
+    text: '#210010',
+    background: '#fff',
+    tint: "#AB1E62",
     tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    tabIconSelected: tintColorLight,
   },
 };
