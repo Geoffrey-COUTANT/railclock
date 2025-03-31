8d95e4dca6dc90fcac94bc33cefc5cdf3c90f921
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2025-03-31 16:48:38 +0200

    final touches before release

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,21 +1,19 @@
 import {
-  StyleSheet,
-  TextInput,
-  Pressable,
-  ScrollView,
-  View, Dimensions, FlatList
+    StyleSheet,
+    TextInput,
+    Pressable,
+    ScrollView,
+    View, Dimensions, FlatList
 } from "react-native";
 import React from "react";
 import {Text} from "@/components/Themed";
 import CardTrain from "@/components/CardTrain";
 import {Ionicons} from '@expo/vector-icons';
 import {getJourneys, getPtObject, searchForPtObject} from "@/app/api/api";
-import { formatTimestamp } from "../api/utils";
+import {formatTimestamp} from "../api/utils";
 
-const trains: any[] | (() => any[]) = [
-
-];
-const { height, width } = Dimensions.get("window");
+const trains: any[] | (() => any[]) = [];
+const {height, width} = Dimensions.get("window");
 
 export default function TabOneScreen() {
     const [departCity, setDepartCity] = React.useState("");
@@ -79,28 +77,30 @@ export default function TabOneScreen() {
             <View style={styles.logoContainer}>
                 <Text style={styles.title}>railclock</Text>
             </View>
-            <TextInput
-                style={styles.input}
-                onChangeText={setDepartCity}
-                value={departCity}
-                placeholder="Gare de départ"
-                placeholderTextColor="#999"
-            />
-            <Pressable onPress={swapCities} style={styles.swapButton}>
-                <Ionicons name="swap-horizontal" size={24} color="#210010"/>
-            </Pressable>
-            <TextInput
-                style={styles.input}
-                onChangeText={setArrivalCity}
-                value={arrivalCity}
-                placeholder="Gare d'arrivée"
-                placeholderTextColor="#999"
-            />
+            <View style={styles.searchBarContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setDepartCity}
+                    value={departCity}
+                    placeholder="Gare de départ"
+                    placeholderTextColor="#999"
+                />
+                <Pressable onPress={swapCities} style={styles.swapButton}>
+                    <Ionicons name="swap-vertical" size={24} color="#210010"/>
+                </Pressable>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setArrivalCity}
+                    value={arrivalCity}
+                    placeholder="Gare d'arrivée"
+                    placeholderTextColor="#999"
+                />
+            </View>
             <Pressable style={styles.button} onPress={handleSearch}>
                 <Text style={styles.buttonText}>Rechercher</Text>
             </Pressable>
             <Text style={styles.sectionTitle}>Mes trajets</Text>
-            {filteredTrains.length === 0 && (<Text>No train found</Text>)}
+            {filteredTrains.length === 0 && (<Text>Aucun train trouvé. Faites une recherche !</Text>)}
             <FlatList
                 contentContainerStyle={styles.contentContainer}
                 data={filteredTrains}
@@ -114,77 +114,85 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    paddingTop: 40,
-    backgroundColor: "#fff",
-    width: "100%",
-  },
-  logoContainer: {
-    marginBottom: 40,
-  },
-  title: {
-    fontSize: 24,
-    color: "#210010",
-    fontFamily: "BricolageGrotesqueSemiBold",
-  },
-  input: {
-    height: 50,
-    borderWidth: 0,
-    padding: 15,
-    width: "90%",
-    borderRadius: 12,
-    backgroundColor: "#f5f5f5",
-    marginBottom: 10,
-    fontSize: 16,
-  },
-  swapButton: {
-    position: "absolute",
-    padding: 10,
-    top: height * 0.145, // 17% de la hauteur de l'écran
-    right: width * 0.03, // 5% du bord droit pour s'adapter à tous les écrans
-    backgroundColor: "white",
-    borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 10,
-  },
-  button: {
-    marginTop: 5,
-    width: "90%",
-    backgroundColor: "#210010",
-    borderRadius: 12,
-    padding: 15,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "600",
-    fontSize: 16,
-  },
-  sectionTitle: {
-    alignSelf: "flex-start",
-    paddingLeft: "5%",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 40,
-    marginBottom: 20,
-  },
-  scrollView: {
-    flex: 1,
-    width: "100%",
-  },
-  contentContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    gap: 10,
-    width: "100%",
-  },
-  noTrainText: {
-    fontSize: 16,
-    textAlign: "center",
-    color: "#AB1E62",
-    marginTop: 20,
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 40,
+        backgroundColor: "#fff",
+        width: "100%",
+    },
+    logoContainer: {
+        marginBottom: 40,
+    },
+    searchBarContainer: {
+        width: "100%",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 24,
+        color: "#210010",
+        fontFamily: "BricolageGrotesqueSemiBold",
+    },
+    subtitle: {
+        fontSize: 20,
+        color: "#210010",
+        fontFamily: "BricolageGrotesqueSemiBold",
+    },
+    input: {
+        height: 50,
+        borderWidth: 0,
+        padding: 15,
+        width: "90%",
+        borderRadius: 12,
+        backgroundColor: "#f5f5f5",
+        marginBottom: 10,
+        fontSize: 16,
+        fontFamily: "BricolageGrotesqueRegular"
+    },
+    swapButton: {
+        position: "absolute",
+        zIndex: 50,
+        right: 15,
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: "100%",
+        top: "28%"
+    },
+    button: {
+        marginTop: 5,
+        width: "90%",
+        backgroundColor: "#210010",
+        borderRadius: 12,
+        padding: 15,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "600",
+        fontSize: 16,
+    },
+    sectionTitle: {
+        alignSelf: "flex-start",
+        paddingLeft: "5%",
+        fontSize: 24,
+        marginTop: 40,
+        marginBottom: 20,
+        fontFamily: "BricolageGrotesqueSemiBold",
+    },
+    scrollView: {
+        flex: 1,
+        width: "100%",
+    },
+    contentContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        gap: 10,
+        width: "100%",
+    },
+    noTrainText: {
+        fontSize: 16,
+        textAlign: "center",
+        color: "#AB1E62",
+        marginTop: 20,
+    }
 });
--- components/CardTrain.tsx
+++ components/CardTrain.tsx
@@ -1,190 +1,193 @@
-import { View, Text, StyleSheet } from "react-native";
-import Svg, { Path } from "react-native-svg";
+import {View, Text, StyleSheet} from "react-native";
+import Svg, {Path} from "react-native-svg";
+import SkeletonContent from 'react-native-skeleton-content';
 
 export default function CardTrain({
-  status = "onTime", // 'onTime', 'delayed', 'cancelled'
-  scheduledTime,
-  actualTime,
-  delayDuration,
-  additionalInfo,
-    depart,
-    arrival
-}: {
-  status?: string;
-  scheduledTime: string;
-  actualTime?: string;
-  delayDuration?: string;
-  additionalInfo?: string;
-  depart?: string;
-  arrival?: string;
+                                      status = "onTime", // 'onTime', 'delayed', 'cancelled'
+                                      scheduledTime,
+                                      actualTime,
+                                      delayDuration,
+                                      additionalInfo,
+                                      depart,
+                                      arrival,
+                                      loading
+                                  }: {
+    status?: string;
+    scheduledTime: string;
+    actualTime?: string;
+    delayDuration?: string;
+    additionalInfo?: string;
+    depart?: string;
+    arrival?: string;
+    loading: boolean;
 }) {
-  const getBackgroundColor = () => {
-    switch (status) {
-      case "delayed":
-        return "#AB1E62";
-      case "cancelled":
-        return "#C54200";
-      default:
-        return "#f5f5f5";
-    }
-  };
+    const getBackgroundColor = () => {
+        switch (status) {
+            case "delayed":
+                return "#AB1E62";
+            case "cancelled":
+                return "#C54200";
+            default:
+                return "#f5f5f5";
+        }
+    };
 
-  return (
-    <View style={[styles.container, { backgroundColor: getBackgroundColor() }]}>
-      <View style={styles.trainContainer}>
-        <Text
-          style={[
-            styles.trainName,
-            status !== "onTime" ? styles.whiteText : styles.blackText,
-          ]}
-        >
-            {depart}
-        </Text>
-        <View style={styles.destinationContainer}>
-          <Svg
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke={status !== "onTime" ? "white" : "black"}
-            strokeWidth={1.5}
-          >
-            <Path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m16.49 12 3.75 3.75m0 0-3.75 3.75m3.75-3.75H3.74V4.499"
-            />
-          </Svg>
-          <Text
-            style={[
-              styles.destination,
-              status !== "onTime" ? styles.whiteText : styles.blackText,
-            ]}
-          >
-              {arrival}
-          </Text>
-        </View>
-      </View>
-      <View style={styles.timeContainer}>
-        {status === "delayed" ? (
-          <>
-            <View style={styles.delayedTimeContainer}>
-              <Text style={styles.scheduledTime}>{scheduledTime}</Text>
-              <Text style={[styles.trainTime, styles.whiteText]}>
-                {actualTime}
-              </Text>
+    return (
+        <View style={[styles.container, {backgroundColor: getBackgroundColor()}]}>
+            <View style={styles.trainContainer}>
+                <Text
+                    style={[
+                        styles.trainName,
+                        status !== "onTime" ? styles.whiteText : styles.blackText,
+                    ]}
+                >
+                    {depart}
+                </Text>
+                <View style={styles.destinationContainer}>
+                    <Svg
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke={status !== "onTime" ? "white" : "black"}
+                        strokeWidth={1.5}
+                    >
+                        <Path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m16.49 12 3.75 3.75m0 0-3.75 3.75m3.75-3.75H3.74V4.499"
+                        />
+                    </Svg>
+                    <Text
+                        style={[
+                            styles.destination,
+                            status !== "onTime" ? styles.whiteText : styles.blackText,
+                        ]}
+                    >
+                        {arrival}
+                    </Text>
+                </View>
             </View>
-            <Text style={styles.delayText}>Retard de {delayDuration}</Text>
-            {additionalInfo && (
-              <Text style={styles.additionalInfo}>{additionalInfo}</Text>
-            )}
-          </>
-        ) : status === "cancelled" ? (
-          <>
-            <Text style={[styles.cancelledText, styles.whiteText]}>
-              SUPPRIMÉ
-            </Text>
-            <Text style={[styles.scheduledTime, styles.whiteText]}>
-              {scheduledTime}
-            </Text>
-            {additionalInfo && (
-              <Text style={styles.additionalInfo}>{additionalInfo}</Text>
-            )}
-          </>
-        ) : (
-          <>
-            <Text style={[styles.trainTime, styles.blackText]}>
-              {scheduledTime}
-            </Text>
-            <Text style={[styles.blackText]}>à l'heure</Text>
-          </>
-        )}
-      </View>
-    </View>
-  );
+            <View style={styles.timeContainer}>
+                {status === "delayed" ? (
+                    <>
+                        <View style={styles.delayedTimeContainer}>
+                            <Text style={styles.scheduledTime}>{scheduledTime}</Text>
+                            <Text style={[styles.trainTime, styles.whiteText]}>
+                                {actualTime}
+                            </Text>
+                        </View>
+                        <Text style={styles.delayText}>Retard de {delayDuration}</Text>
+                        {additionalInfo && (
+                            <Text style={styles.additionalInfo}>{additionalInfo}</Text>
+                        )}
+                    </>
+                ) : status === "cancelled" ? (
+                    <>
+                        <Text style={[styles.cancelledText, styles.whiteText]}>
+                            SUPPRIMÉ
+                        </Text>
+                        <Text style={[styles.scheduledTime, styles.whiteText]}>
+                            {scheduledTime}
+                        </Text>
+                        {additionalInfo && (
+                            <Text style={styles.additionalInfo}>{additionalInfo}</Text>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <Text style={[styles.trainTime, styles.blackText]}>
+                            {scheduledTime}
+                        </Text>
+                        <Text style={[styles.blackText]}>à l'heure</Text>
+                    </>
+                )}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: undefined,
-    alignSelf: "stretch",
-    marginVertical: 2,
-    width: "100%",
-    borderRadius: 10,
-    padding: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#f5f5f5",
-  },
-  trainName: {
-    fontSize: 20,
-    fontFamily: "BricolageGrotesqueSemiBold",
-  },
-  trainTime: {
-    fontSize: 30,
-    lineHeight: 30,
-    fontFamily: "BricolageGrotesqueSemiBold"
-  },
-  trainContainer: {
-    flexDirection: "column",
-    fontSize: 18,
-  },
-  destinationContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    fontSize: 18,
-  },
-  destination: {
-    fontSize: 20,
-    fontFamily: "BricolageGrotesqueSemiBold",
-  },
-  timeContainer: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    fontSize: 18,
-    alignItems: "flex-end",
-    flex: 1,
-    marginLeft: 10,
-  },
-  delayedTimeContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    flexWrap: "wrap",
-    justifyContent: "flex-end",
-  },
-  scheduledTime: {
-    fontSize: 18,
-    textDecorationLine: "line-through",
-    fontWeight: "bold",
-    color: "white",
-  },
-  delayText: {
-    fontSize: 16,
-    color: "white",
-  },
-  cancelledText: {
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  whiteText: {
-    color: "#fff",
-  },
-  blackText: {
-    color: "#000",
-  },
-  additionalInfo: {
-    fontSize: 12,
-    color: "white",
-    marginTop: 4,
-    fontStyle: "italic",
-    textAlign: "left",
-    flexWrap: "nowrap",
-  },
-  stopNameText: {
-    fontSize: 20,
-    fontFamily: "BricolageGrotesqueSemiBold",
-  }
+    container: {
+        flex: undefined,
+        alignSelf: "stretch",
+        marginVertical: 2,
+        width: "100%",
+        borderRadius: 10,
+        padding: 15,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#f5f5f5",
+    },
+    trainName: {
+        fontSize: 20,
+        fontFamily: "BricolageGrotesqueSemiBold",
+    },
+    trainTime: {
+        fontSize: 30,
+        lineHeight: 30,
+        fontFamily: "BricolageGrotesqueSemiBold"
+    },
+    trainContainer: {
+        flexDirection: "column",
+        fontSize: 18,
+    },
+    destinationContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+        fontSize: 18,
+    },
+    destination: {
+        fontSize: 20,
+        fontFamily: "BricolageGrotesqueSemiBold",
+    },
+    timeContainer: {
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontSize: 18,
+        alignItems: "flex-end",
+        flex: 1,
+        marginLeft: 10,
+    },
+    delayedTimeContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+        flexWrap: "wrap",
+        justifyContent: "flex-end",
+    },
+    scheduledTime: {
+        fontSize: 18,
+        textDecorationLine: "line-through",
+        fontWeight: "bold",
+        color: "white",
+    },
+    delayText: {
+        fontSize: 16,
+        color: "white",
+    },
+    cancelledText: {
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    whiteText: {
+        color: "#fff",
+    },
+    blackText: {
+        color: "#000",
+    },
+    additionalInfo: {
+        fontSize: 12,
+        color: "white",
+        marginTop: 4,
+        fontStyle: "italic",
+        textAlign: "left",
+        flexWrap: "nowrap",
+    },
+    stopNameText: {
+        fontSize: 20,
+        fontFamily: "BricolageGrotesqueSemiBold",
+    }
 });
